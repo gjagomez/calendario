4efc90cf4cd6951003997a02481db605bad53626
gjagomez <36309181+gjagomez@users.noreply.github.com>
2022-09-02 17:14:52 -0600

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,97 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Fragment, useState } from "react";
+import { Button, Typography } from "@mui/material";
+import { Scheduler } from "@aldabil/react-scheduler";
+import { RESOURCES, EVENTS } from "./data";
+import PersonRoundedIcon from "@mui/icons-material/PersonRounded";
 
 function App() {
+  const [mode, setMode] = useState<"default" | "tabs">("default");
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Fragment>
+    <div style={{ textAlign: "center" }}>
+      <span>Resource View Mode: </span>
+      <Button
+        color={mode === "default" ? "primary" : "inherit"}
+        variant={mode === "default" ? "contained" : "text"}
+        size="small"
+        onClick={() => setMode("default")}
+      >
+        Default
+      </Button>
+      <Button
+        color={mode === "tabs" ? "primary" : "inherit"}
+        variant={mode === "tabs" ? "contained" : "text"}
+        size="small"
+        onClick={() => setMode("tabs")}
+      >
+        Tabs
+      </Button>
     </div>
+
+    
+    <Scheduler
+      events={EVENTS}
+      resources={RESOURCES}
+      week={{
+        weekDays: [0, 1, 2, 3, 4, 5],
+        weekStartOn: 6,
+        startHour: 9,
+        endHour: 18,
+        step: 60
+      }}
+      resourceFields={{
+        idField: "admin_id",
+        textField: "title",
+        subTextField: "mobile",
+        avatarField: "title",
+        colorField: "color"
+      }}
+      resourceViewMode={mode}
+      selectedDate={new Date(2021, 4, 2)}
+      fields={[
+        {
+          name: "admin_id",
+          type: "select",
+          default: RESOURCES[0].admin_id,
+          options: RESOURCES.map((res) => {
+            return {
+              id: res.admin_id,
+              text: `${res.title} (${res.mobile})`,
+              value: res.admin_id //Should match "name" property
+            };
+          }),
+          config: { label: "Assignee", required: true }
+        }
+      ]}
+      viewerExtraComponent={(fields, event) => {
+        return (
+          <div>
+            {fields.map((field, i) => {
+              if (field.name === "admin_id") {
+                // const admin =field.options.find(
+                //   (fe) => fe.id === event.admin_id
+                // );
+                return (
+                  <Typography
+                    key={i}
+                    style={{ display: "flex", alignItems: "center" }}
+                    color="textSecondary"
+                    variant="caption"
+                    noWrap
+                  >
+                    <PersonRoundedIcon /> {"admin.text"}
+                  </Typography>
+                );
+              } else {
+                return "";
+              }
+            })}
+          </div>
+        );
+      }}
+    />
+  </Fragment>
   );
 }
 
---
+++ src/data.js
@@ -0,0 +1,143 @@
+export const EVENTS = [
+  {
+    event_id: 1,
+    title: "Masaje",
+    start: new Date("2021 5 2 09:30"),
+    end: new Date("2021 5 2 10:00"),
+    admin_id: 1
+  },
+  {
+    event_id: 1,
+    title: "Maquinaria",
+    start: new Date("2021 5 2 10:00"),
+    end: new Date("2021 5 2 10:30"),
+    admin_id: 1
+  },
+  {
+    event_id: 3,
+    title: "Event 3",
+    start: new Date("2021 4 27 09:00"),
+    end: new Date("2021 4 28 10:00"),
+    admin_id: 1
+  },
+  {
+    event_id: 4,
+    title: "Event 4",
+    start: new Date("2021 5 4 9:00"),
+    end: new Date("2021 5 4 10:36"),
+    admin_id: 2
+  },
+  {
+    event_id: 5,
+    title: "Event 5",
+    start: new Date("2021 5 1 10:00"),
+    end: new Date("2021 5 18 11:00"),
+    admin_id: 4
+  },
+  {
+    event_id: 6,
+    title: "Event 6",
+    start: new Date("2021 5 2 11:00"),
+    end: new Date("2021 5 2 12:00"),
+    admin_id: 2
+  },
+  {
+    event_id: 7,
+    title: "Event 7",
+    start: new Date("2021 5 1 12:00"),
+    end: new Date("2021 5 1 13:00"),
+    admin_id: 3
+  },
+  {
+    event_id: 8,
+    title: "Event 8",
+    start: new Date("2021 5 1 13:00"),
+    end: new Date("2021 5 1 14:00"),
+    admin_id: 4
+  },
+  {
+    event_id: 9,
+    title: "Event 11",
+    start: new Date("2021 5 5 16:00"),
+    end: new Date("2021 5 5 17:00"),
+    admin_id: 1
+  },
+  {
+    event_id: 10,
+    title: "Event 9",
+    start: new Date("2021 5 6  15:00"),
+    end: new Date("2021 5 6 16:00"),
+    admin_id: 2
+  },
+  {
+    event_id: 11,
+    title: "Event 10",
+    start: new Date("2021 5 6 14:00"),
+    end: new Date("2021 5 6 15:00"),
+    admin_id: 1
+  }
+];
+
+export const RESOURCES = [
+  {
+    admin_id: 1,
+    title: "John",
+    mobile: "555666777",
+    avatar: "https://picsum.photos/200/300",
+    color: "#ab2d2d"
+  },
+  {
+    admin_id: 2,
+    title: "Sarah",
+    mobile: "545678354",
+    avatar: "https://picsum.photos/200/300",
+    color: "#58ab2d"
+  },
+  {
+    admin_id: 3,
+    title: "Joseph",
+    mobile: "543678433",
+    avatar: "https://picsum.photos/200/300",
+    color: "#a001a2"
+  },
+  {
+    admin_id: 4,
+    title: "Mera",
+    mobile: "507487620",
+    avatar: "https://picsum.photos/200/300",
+    color: "#08c5bd"
+  }
+  ,
+  {
+    admin_id: 5,
+    title: "Javier",
+    mobile: "507487620",
+    avatar: "https://picsum.photos/200/300",
+    color: "#08c5bd"
+  }
+  ,
+  {
+    admin_id: 6,
+    title: "Veronica",
+    mobile: "507487620",
+    avatar: "https://picsum.photos/200/300",
+    color: "#08c5bd"
+  }
+  ,
+  {
+    admin_id: 7,
+    title: "Melanie",
+    mobile: "507487620",
+    avatar: "https://picsum.photos/200/300",
+    color: "#08c5bd"
+  }
+  ,
+  {
+    admin_id: 8,
+    title: "Jonathan",
+    mobile: "507487620",
+    avatar: "https://picsum.photos/200/300",
+    color: "#08c5bd"
+  }
+  
+];
